3c7f2e8fd2b292c938d9d5c0815c4e6ea0d79197
Bo Andree <b.p.j.andree@vu.nl>
2024-06-28 16:08:21 +0200

    Update get_regids.R
    minor textual edits to comments, added that there is a str import, changed the if statements to use an OR operator for readability

--- R/get_regids.R
+++ R/get_regids.R
@@ -1,38 +1,43 @@
-#' @title Printing ISO 3166-2 Region Codes
+#' @title Retrieve ISO 3166-2 Region Codes
 #'
-#' @description This function returns a data frame containing ISO 3166-2 region
-#' codes.
+#' @description This function returns a data frame containing ISO 3166-2 region codes for specified administrative levels.
 #'
 #' @param admin_lvl [optional] (numeric) Specifies the administrative level. Use
 #' \code{0} for country-level, \code{1} for state-level, or \code{NULL} to
 #' include all regions (default: \code{0}).
 #'
-#' @return A data frame object that includes region names, ISO 3166-2 codes, and
-#' administrative level.
+#' @return A data frame containing region names, ISO 3166-2 codes, and
+#' administrative levels.
 #'
 #' @export
 #'
 #' @examples
-#' # Example: Storing ISO 3166-2 region codes
+#' # Example: Retrieve ISO 3166-2 region codes
 #' \dontrun{
-#'  regids <- get_regids()
+#'   regids <- get_regids()
+#'   print(regids)
 #' }
 #'
 #' @importFrom magrittr %>%
 #' @importFrom sf st_set_geometry
 #' @importFrom rnaturalearth ne_states ne_countries
 #' @importFrom dplyr select rename filter mutate arrange
+#' @importFrom stringr str_detect
 #'
 get_regids <- function(admin_lvl = 0) {
 
   # To avoid 'no visible binding for global variable' messages (CRAN test)
   iso <- name <- iso_a2_eh <- iso_3166_2 <- geounit <- NULL
 
-  if (!any(is.null(admin_lvl), (admin_lvl %in% c(0, 1)))) {
+  # Validate 'admin_lvl' parameter
+  if (!is.null(admin_lvl) && !admin_lvl %in% c(0, 1)) {
     stop("Invalid 'admin_lvl' parameter. Valid entries are 0, 1, or NULL.")
   }
 
-  if (any(admin_lvl == 0, is.null(admin_lvl))){
+  regions <- NULL
+
+  # Retrieve country-level regions if admin_lvl is 0 or NULL
+  if (is.null(admin_lvl) || admin_lvl == 0) {
     regions <- ne_countries(scale = "small") %>%
       st_set_geometry(NULL) %>%
       select(geounit, iso_a2_eh) %>%
@@ -41,7 +46,8 @@ get_regids <- function(admin_lvl = 0) {
       mutate(admin_lvl = 0)
   }
 
-  if (any(admin_lvl == 1, is.null(admin_lvl))){
+  # Retrieve state-level regions if admin_lvl is 1 or NULL
+  if (is.null(admin_lvl) || admin_lvl == 1) {
     states <- ne_states() %>%
       st_set_geometry(NULL) %>%
       select(name, iso_3166_2) %>%
@@ -55,6 +61,7 @@ get_regids <- function(admin_lvl = 0) {
     }
   }
 
+  # Arrange the regions data frame by administrative level and ISO code
   regions <- regions %>% arrange(admin_lvl, iso)
 
   return(regions)
